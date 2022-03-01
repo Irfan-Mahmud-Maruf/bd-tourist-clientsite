3e4d99893455adc8ffe56fdcb7f6205621cb6427
Irfan Mahmuf Maruf <67922561+Irfan-Mahmud-Maruf@users.noreply.github.com>
2022-03-02 02:54:23 +0600

    color correction

--- src/components/home/AboutUs.js
+++ src/components/home/AboutUs.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import { BsFillCheckCircleFill } from "react-icons/bs";
+
 
 const AboutUs = () => {
     return (
         <section className='be-faq'>
-            <Container className="be-section-header text-center mt-5">
+            <Container className="text-center mt-5">
                 <h1 >About us</h1>
             </Container>
 
@@ -14,14 +16,27 @@ const AboutUs = () => {
                 <Col lg={6} md={6} sm={12}>
                     <h3>BD Tourist</h3>
                     <br />
-                    <p>BD Tourist help all the tourist who wanna visit Bangladesh. Welcome visitors at Bangladesh.</p>
-                    <br />
+                    <div className='d-flex justify-content-center align-items-start mb-2'>
+                    <div  className='me-3'>
+                    <BsFillCheckCircleFill/>
+                    </div>
+                    <p>BD Tourist help all the tourist who wanna visit Bangladesh. Welcome visitors at Bangladesh. </p>
+                    </div>
+                    <div className='d-flex justify-content-center align-items-start mb-2'>
+                    <div  className='me-3'>
+                    <BsFillCheckCircleFill/>
+                    </div>
                     <p>BD Tourist have flexible packages which suite your budget. We are soft for payment return policy. So not to be worried if anyone miss his tour.</p>
-                    <br />
+                    </div>
+                    <div className='d-flex justify-content-center align-items-start mb-2'>
+                    <div className='me-3'>
+                    <BsFillCheckCircleFill/>
+                    </div>
                     <p>BD Tourist make sure your safety first. You will 24 hours guide. They will update all kind of news about your tour</p>
-                    <br />
+                    </div>
                     <p>Lets explore the history of Bangladesh and the beauty of Bangladesh with us. </p>
                 </Col>
+               
                 <Col lg={5} md={5} sm={12}>
                     <img src={`https://img.freepik.com/free-vector/back-back-concept-illustration_114360-5999.jpg?w=740`} alt="" className='w-100'/>
                 </Col>
--- src/components/shared/Navigation/Footer.js
+++ src/components/shared/Navigation/Footer.js
@@ -1,24 +1,97 @@
-import { Col, Container, Row } from 'react-bootstrap'
 
 
 
 const Footer = () => {
     return (
         <>
-            <footer>
-                <Container className="pt-5 mb-5 text-center" >
-                    <h1>BD Tourist</h1>
-                    <p>Visit Bangladesh with Us</p>
-                </Container>
+            <footer
+          class="text-center text-lg-start text-white"
+        //   style="background-color: #929fba"
+          >
+    
+    <div class="container p-4 pb-0">
+      <section class="">
+        <div class="row">
+          <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">
+              Company name
+            </h6>
+            <p>
+              Here you can use rows and columns to organize your footer
+              content. Lorem ipsum dolor sit amet, consectetur adipisicing
+              elit.
+            </p>
+          </div>
 
-                <Container className="copyright pt-2">
-                    <Row className="text-center">
-                        <Col>
-                            <p>Copyright 2021. All rights reserved.</p>
-                        </Col>
-                    </Row>
-                </Container>
-            </footer>  
+          
+{/* 
+          <hr class="w-100 clearfix d-md-none" />
+          <hr class="w-100 clearfix d-md-none" /> */}
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+            <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+            <p><i class="fas fa-envelope mr-3"></i> info@gmail.com</p>
+            <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+            <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+          </div>
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">Follow us</h6>
+            <a
+               class="btn btn-primary btn-floating m-1"
+            //    style="background-color: #3b5998"
+               href="#!"
+               role="button"
+               ><i class="fab fa-facebook-f"></i
+              ></a>
+            <a
+               class="btn btn-primary btn-floating m-1"
+            //    style="background-color: #55acee"
+               href="#!"
+               role="button"
+               ><i class="fab fa-twitter"></i
+              ></a>
+            <a
+               class="btn btn-primary btn-floating m-1"
+            //    style="background-color: #dd4b39"
+               href="#!"
+               role="button"
+               ><i class="fab fa-google"></i
+              ></a>
+            <a
+               class="btn btn-primary btn-floating m-1"
+            //    style="background-color: #ac2bac"
+               href="#!"
+               role="button"
+               ><i class="fab fa-instagram"></i
+              ></a>
+            <a
+               class="btn btn-primary btn-floating m-1"
+            //    style="background-color: #0082ca"
+               href="#!"
+               role="button"
+               ><i class="fab fa-linkedin-in"></i
+              ></a>
+            <a
+               class="btn btn-primary btn-floating m-1"
+            //    style="background-color: #333333"
+               href="#!"
+               role="button"
+               ><i class="fab fa-github"></i
+              ></a>
+          </div>
+        </div>
+      </section>
+    </div>
+    <div
+         class="text-center p-3"
+        //  style="background-color: rgba(0, 0, 0, 0.2)"
+         >
+      © 2020 Copyright:
+      <a class="text-white" href="https://mdbootstrap.com/"
+         >MDBootstrap.com</a
+        >
+    </div>
+  </footer>
         </>
     )
 }
