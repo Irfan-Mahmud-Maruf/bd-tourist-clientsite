3310ca193b8d5ac8148a95755f93ef451300aff4
Irfan Mahmuf Maruf <67922561+Irfan-Mahmud-Maruf@users.noreply.github.com>
2022-03-02 11:16:30 +0600

    footer updated

--- src/components/shared/Navigation/Footer.js
+++ src/components/shared/Navigation/Footer.js
@@ -1,97 +1,72 @@
+import { Container, Row } from "react-bootstrap";
+import { BsEnvelopeFill, BsFacebook, BsGithub, BsGoogle, BsHouseFill, BsInstagram, BsLinkedin, BsPrinterFill, BsTelephoneFill, BsTwitter } from "react-icons/bs";
 
 
 
 const Footer = () => {
     return (
         <>
-            <footer
-          class="text-center text-lg-start text-white"
-        //   style="background-color: #929fba"
-          >
+          <footer className="text-center text-lg-start">
     
-    <div class="container p-4 pb-0">
-      <section class="">
-        <div class="row">
-          <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-            <h6 class="text-uppercase mb-4 font-weight-bold">
-              Company name
-            </h6>
-            <p>
-              Here you can use rows and columns to organize your footer
-              content. Lorem ipsum dolor sit amet, consectetur adipisicing
-              elit.
-            </p>
-          </div>
+            <Container className="p-5 pb-0">
+                <Row>
+                  <div className="col-md-4 col-lg-4 col-sm-12 mx-auto mt-3">
+                    <h6 class="text-uppercase mb-4 font-weight-bold">
+                      BD Tourist
+                    </h6>
+                    <p>
+                      BD Tourist help all the tourist who wanna visit Bangladesh. Welcome visitors at Bangladesh.
+                    </p>
+                  </div>
 
-          
-{/* 
-          <hr class="w-100 clearfix d-md-none" />
-          <hr class="w-100 clearfix d-md-none" /> */}
-          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-            <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
-            <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-            <p><i class="fas fa-envelope mr-3"></i> info@gmail.com</p>
-            <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-            <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
-          </div>
-          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-            <h6 class="text-uppercase mb-4 font-weight-bold">Follow us</h6>
-            <a
-               class="btn btn-primary btn-floating m-1"
-            //    style="background-color: #3b5998"
-               href="#!"
-               role="button"
-               ><i class="fab fa-facebook-f"></i
-              ></a>
-            <a
-               class="btn btn-primary btn-floating m-1"
-            //    style="background-color: #55acee"
-               href="#!"
-               role="button"
-               ><i class="fab fa-twitter"></i
-              ></a>
-            <a
-               class="btn btn-primary btn-floating m-1"
-            //    style="background-color: #dd4b39"
-               href="#!"
-               role="button"
-               ><i class="fab fa-google"></i
-              ></a>
-            <a
-               class="btn btn-primary btn-floating m-1"
-            //    style="background-color: #ac2bac"
-               href="#!"
-               role="button"
-               ><i class="fab fa-instagram"></i
-              ></a>
-            <a
-               class="btn btn-primary btn-floating m-1"
-            //    style="background-color: #0082ca"
-               href="#!"
-               role="button"
-               ><i class="fab fa-linkedin-in"></i
-              ></a>
-            <a
-               class="btn btn-primary btn-floating m-1"
-            //    style="background-color: #333333"
-               href="#!"
-               role="button"
-               ><i class="fab fa-github"></i
-              ></a>
-          </div>
-        </div>
-      </section>
-    </div>
-    <div
-         class="text-center p-3"
-        //  style="background-color: rgba(0, 0, 0, 0.2)"
-         >
-      © 2020 Copyright:
-      <a class="text-white" href="https://mdbootstrap.com/"
-         >MDBootstrap.com</a
-        >
-    </div>
-  </footer>
+                  <div className="col-md-4 col-lg-4 col-sm-12 mx-auto mt-3">
+                    <h6 className="text-uppercase mb-4 font-weight-bold">Contact</h6>
+                    <p><BsHouseFill className="me-3"/> Dhaka, Bangladesh.</p>
+                    <p><BsEnvelopeFill className="me-3"/> info@bdtourist.com</p>
+                    <p><BsTelephoneFill className="me-3"/> + 01 234 567 88</p>
+                    <p><BsPrinterFill className="me-3"/> + 01 234 567 88</p>
+                  </div>
+                  <div className="col-md-4 col-lg-4 col-sm-12 mx-auto mt-3">
+                    <h6 class="text-uppercase mb-4 font-weight-bold">Follow us</h6>
+                    <a 
+                       className="btn btn-primary m-1 rounded-circle"
+                       href="#!"
+                       role="button"
+                       ><BsFacebook/></a>
+                    <a
+                       class="btn btn-primary m-1 rounded-circle"
+                       href="#!"
+                       role="button"
+                       ><BsTwitter/></a>
+                    <a
+                       class="btn btn-primary m-1 rounded-circle"
+                       href="#!"
+                       role="button"
+                       ><BsGoogle/></a>
+                    <a
+                       class="btn btn-primary m-1 rounded-circle"
+                       href="#!"
+                       role="button"
+                       ><BsInstagram/></a>
+                    <a
+                       class="btn btn-primary m-1 rounded-circle"
+                       href="#!"
+                       role="button"
+                       ><BsLinkedin/></a>
+                    <a
+                       class="btn btn-primary m-1 rounded-circle"
+                       href="#!"
+                       role="button"
+                       ><BsGithub/></a>
+                  </div>
+                </Row>
+            </Container>
+            <hr />
+            <div class="text-center p-3">
+              © 2020 Copyright:
+              <span className="text-muted">BD Tourist</span>
+            </div>
+          </footer>
         </>
     )
 }
