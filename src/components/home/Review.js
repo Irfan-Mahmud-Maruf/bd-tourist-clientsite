d791d7d47a69723253a7f88c28421f2c0af5e480
Irfan Mahmuf Maruf <67922561+Irfan-Mahmud-Maruf@users.noreply.github.com>
2022-03-04 19:31:46 +0600

    error fixing

--- src/components/home/Review.js
+++ src/components/home/Review.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { Card, Col, Container, Row } from 'react-bootstrap';
-import { DynamicStar } from 'react-dynamic-star';
+import { BsHeartFill } from "react-icons/bs";
+import Rating from 'react-rating';
 import person from "./../../img/person.png";
 
 const Review = () => {
     const [reviews, setReviews] = useState()
 
     useEffect(() => {
-        const url = `${process.env.REACT_APP_API_URL}/reviews?limit=3`;
+        const url = `${process.env.REACT_APP_API_URL}/reviews`;
 
         fetch(url).then(res=> res.json()).then(data=>setReviews(data))
     }, [])
@@ -17,12 +18,16 @@ const Review = () => {
             <Row className='d-flex justify-content-center align-items-center'>
                 {reviews && reviews.map(review =>(
                     <Col lg={4} md={4} sm={12} key={review._id}>
-                        <Card className="mb-4">
-                            <Card.Img src={person}/>
-                            <Card.Body>
+                        <Card className="mb-4 reviews d-flex justify-content-center align-items-center flex-column">
+                            <img src={person} alt="" className=''/>
+                            <Card.Body className='text-center'>
                                 <Card.Title>{review.username}</Card.Title>
                                 <Card.Text >{review.reviews}</Card.Text>
-                                <DynamicStar rating={review.rating} className="w-25"/>
+                                <Rating className='icon-color' 
+                                    readonly
+                                    initialRating={review.rating}
+                                    emptySymbol={BsHeartFill}
+                                    fullSymbol={BsHeartFill}/>
                             </Card.Body>
                         </Card>
                     </Col>)
